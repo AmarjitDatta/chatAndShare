8ec06d58bc740c5e97d68d4834e3f93a8a222746
Rajeeb Saha <rajeebsaha.dev@gmail.com>
2015-03-10 16:48:36 +0600

    From struts action, we can now get user information

--- src/com/chat/actions/GetUserData.java
+++ src/com/chat/actions/GetUserData.java
@@ -2,10 +2,13 @@ package com.chat.actions;
 
 import com.chat.database.util.DatabaseUtil;
 import com.opensymphony.xwork2.ActionSupport;
+import org.apache.commons.lang.xwork.StringUtils;
 
 public class GetUserData extends ActionSupport {
   private String userName;
-  private String userInformation;
+  private String latitude;
+  private String longitude;
+  private String ipAddress;
 
   public String getUserName() {
     return this.userName;
@@ -15,12 +18,27 @@ public class GetUserData extends ActionSupport {
     this.userName = pUserName;
   }
 
-  public String getUserInformation() {
-    return this.userInformation;
+  public String getLatitude() {
+    return this.latitude;
+  }
+
+  public String getLongitude() {
+    return this.longitude;
+  }
+
+  public String getIpAddress() {
+    return this.ipAddress;
   }
 
   public String execute() throws Exception {
-    userInformation = DatabaseUtil.getInstance().getUserInformation(getUserName());
+    String userInformation = DatabaseUtil.getInstance().getUserInformation(getUserName());
+    if (StringUtils.isNotBlank(userInformation)) {
+      String[] informationArray = userInformation.split(",");
+      ipAddress = informationArray[1];
+      latitude = informationArray[2];
+      longitude = informationArray[3];
+    }
+
     return "success";
   }
 }
